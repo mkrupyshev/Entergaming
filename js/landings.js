3a5346e10a5b7952779fcaa43362112f9c3aad2c
Webcthulhu <stardust>
2016-02-01 13:24:11 +0200

    Mobile popup menu - closes on click outside the menu.

--- js/landings.js
+++ js/landings.js
@@ -1,9 +1,20 @@
 (function($){
+    var $html = $('html');
     var $menuButton = $('#menu-button');
     var $menuPopup = $('#menu-popup');
 
-    $menuButton.on('click', function (){
-        $(this).toggleClass('active');
+    $menuButton.on('click', function (e){
+        e.stopPropagation();
+        $menuButton.toggleClass('active');
         $menuPopup.toggleClass('active');
+    });
+    $menuPopup.on('click', function (e){
+        e.stopPropagation();
+    });
+    $html.on('click', function() {
+        if ($menuPopup.hasClass('active')) {
+            $menuButton.removeClass('active');
+            $menuPopup.removeClass('active');
+        }
     });
 })($);
